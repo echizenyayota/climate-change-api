38dc88510f83aad819806fb287e79b25b4fc54c8
echizenyayota <virtualmisso30@gmail.com>
2023-07-06 17:35:58 +0900

    19:38 routing in Express

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const app = express();
+app.get("/", (req, res) => {
+  res.json("Welcome to my Climate Change News API");
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
