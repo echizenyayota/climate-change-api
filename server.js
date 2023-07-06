f15df77cf74d82f61766344557e863c53599a368
echizenyayota <virtualmisso30@gmail.com>
2023-07-06 17:58:25 +0900

    21:44 The guardian website coming back to us

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const app = express();
+
 app.get("/", (req, res) => {
   res.json("Welcome to my Climate Change News API");
-})
+});
+
+app.get("/news", (req, res) => {
+  axios.get("http://www.theguardian.com/environment/climate-crisis")
+    .then((response) => {
+      const html = response.data;
+      console.log(html);
+    });
+  
+});
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
